8be31a60bcf97718fbcaf0f12af5a931e0c82e44
Hloni <hramakatane@gmail.com>
2020-12-23 17:06:16 +0200

    2nd Commit - Card using justify-content-center

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
 import React from "react";
 import SearchBar from "./SearchBar";
 import Card from "./Card";
 import Pagination from "./Pagination";
-// //  import { Route, Switch } from "react-router";
 
-const useStyles = makeStyles({});
+// //  import { Route, Switch } from "react-router";
+// <div style={{ width: '100%' }}>
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+  },
+});
 export default function App() {
   const classes = useStyles();
 
   return (
     <div className={classes.container}>
       <SearchBar />
-      <Card />
+      <Box
+        display="flex"
+        justifyContent="center"
+        m={1}
+        p={1}
+        bgcolor="background.white"
+      >
+        <Box p={1} bgcolor="white.300">
+          <Card />
+        </Box>
+        <Box p={1} bgcolor="white.300">
+          <Card />
+        </Box>
+        <Box p={1} bgcolor="white.300">
+          <Card />
+        </Box>
+      </Box>
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
@@ -25,21 +25,22 @@ export default function MediaCard() {
       <CardActionArea>
         <CardMedia
           className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
+          //  Image input.. 
+          image="https://images.unsplash.com/photo-1546900703-cf06143d1239?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1291&q=80"
+          title="First Hackathon!"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Lizard
+            First Hackathon
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
+            SpyderWebStudios Co-Founder, Lehlohonolo Ramakatane competted in his
+            first ever Hackathon!
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button size="small" color="secondary">
           Share
         </Button>
         <Button size="small" color="primary">
